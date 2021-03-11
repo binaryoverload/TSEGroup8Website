53cf376be58cd5e59cbf9e989696d1286f9dc150
William Oldham <wegg7250@gmail.com>
2021-03-11 10:41:29 +0000

    Map!

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,render_template
+app = Flask(__name__, static_url_path="/static", static_folder='static/')
 
-
-@app.route('/')
+@app.route('/map')
 def hello():
-    return "Hello World!"
+    return render_template("map.html")
 
 if __name__ == '__main__':
     app.run()
