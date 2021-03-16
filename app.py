5ad7f4571a0cae80210913b7e56395f09e8cbb29
William Oldham <wegg7250@gmail.com>
2021-03-16 14:33:26 +0000

    Integrate static website

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask,render_template
 app = Flask(__name__, static_url_path="/static", static_folder='static/')
 
 @app.route('/map')
-def hello():
+def map():
     return render_template("map.html")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
