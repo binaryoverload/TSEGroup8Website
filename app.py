b9be818e0f1d0bf32c0c1b1550140fb3ba81d502
William Oldham <wegg7250@gmail.com>
2021-03-26 09:48:56 +0000

    Remove old map.html, not needed

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask,render_template
 app = Flask(__name__, static_url_path="/static", static_folder='static/')
 
-@app.route('/map')
-def map():
-    return render_template("map.html")
-
 @app.route('/')
 def index():
     return render_template("index.html")
