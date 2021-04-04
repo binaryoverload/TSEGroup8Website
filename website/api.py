03c0ab59364ca2482e5619d917b2f2ce81cef66e
William Oldham <wegg7250@gmail.com>
2021-04-04 17:51:50 +0100

    Modify API Route

--- website/api.py
+++ website/api.py
@@ -4,7 +4,12 @@ from website import app
 from website.data import countyCodes as counties
 from website.utils import docache
 
-@app.route('/countyCodes')
+@app.route('/counties', methods=['GET'])
 @docache(minutes=60*24*5) # Cache for 5 days since this will not change!
 def countyCodes():
     return json.dumps(counties)
+
+@app.route('/counties/list', methods=['GET'])
+@docache(minutes=60*24*5) # Cache for 5 days since this will not change!
+def countyList():
+    return json.dumps(list(counties.keys()))
